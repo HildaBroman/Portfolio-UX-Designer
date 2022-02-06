c3772810069e02e98f57902d66272434868a6982
Hilda Broman <hilda.broman@chasacademy.se>
2022-02-06 21:17:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//** Denna funktion gör om min profilbild på indexsidan till en annan bild */
+
+function changeImage() {
+
+    if (document.getElementById("imgClickAndChange").src == "hildacitat.png") 
+    {
+        document.getElementById("imgClickAndChange").src = "hildacitat.png";
+    }
+    else 
+    {
+        document.getElementById("imgClickAndChange").src = "hildacitat.png";
+    }
+}
